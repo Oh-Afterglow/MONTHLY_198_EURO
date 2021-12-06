f0bd7450d85fd5723062e893445722ccc76f3c9c
enzymii <enzymiix@gmail.com>
2021-12-06 15:46:37 +0800

    ✨  [front] add dashboard and signin template

---
+++ Code/Frontend/src/App.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+
+function Copyright() {
+  return (
+    <Typography variant='body2' color='text.secondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
