98e9572b720e2f5af9b84cb6b778209c18eaad04
enzymii <enzymiix@gmail.com>
2021-12-17 20:02:54 +0800

    🔨  replace datagrid with table in project dashboard

--- Code/Frontend/src/components/ProjectTable.js
+++ Code/Frontend/src/components/ProjectTable.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 const ProjectTable = () => {
   const cols = [
@@ -17,14 +23,25 @@ const ProjectTable = () => {
     { id: 4, type: 'Bug', time: '10:00', author: 'John', status: 'Open' },
   ];
 
+  const tableHead = cols.map((col) => (
+    <TableCell key={col.field}>{col.headerName}</TableCell>
+  ));
+
+  const tableRows = rows.map((row) => {
+    const cells = cols.map((col) => (
+      <TableCell key={col.field + row.id}>{row[col.field]}</TableCell>
+    ));
+    return <TableRow key={row.id}>{cells}</TableRow>;
+  });
+
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={cols}
-        pageSize={5}
-        owsPerPageOptions={[5]}
-      />
+      <Table>
+        <TableHead>
+          <TableRow>{tableHead}</TableRow>
+        </TableHead>
+        <TableBody>{tableRows}</TableBody>
+      </Table>
     </div>
   );
 };
