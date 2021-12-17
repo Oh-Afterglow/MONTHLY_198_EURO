1d01f1d79cbab520363f7e1a28c659b110654f14
-CLOWN <2766838022@qq.com>
2021-12-17 19:55:18 +0800

    Add member  projects
    projects display

---
+++ Code/Frontend/src/components/ProjectCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import StarIcon from '@mui/icons-material/Star';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const ProjectCard = ({ name,  description ,major,star,lastupdate}) => {
+  const cardStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    padding: '1rem 2rem 0 2rem',
+    marginTop: '1rem',
+    minHeight: '8rem',
+  };
+
+
+
+  return (
+    <Card style={cardStyle}>
+      <div  style={{display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
+        <div>
+            <Typography variant='h5'>{name}<Chip label="Public" variant="outlined" size="small"  style={{marginLeft:"10px",marginBottom:"2px"}}/></Typography>
+            <Typography variant='body2'  style={{opacity:"0.9"}}>{description}</Typography>
+        </div>
+        <div style={{display:"flex",alignItems:"center",marginBottom:"8px"}} >
+            <GitHubIcon style={{opacity:"0.5"}}/>
+            <Typography variant='body1'style={{marginLeft:"10px"}}>{major}</Typography>
+            <StarIcon style={{opacity:"0.3" ,marginLeft:"10px"}}/>
+            <Typography variant='body1'style={{marginLeft:"5px"}} >{star}</Typography>
+            <Typography variant="caption" style={{marginLeft:"20px",opacity:"0.8" ,}}>{lastupdate}</Typography>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectCard;
--- Code/Frontend/src/pages/Member.js
+++ Code/Frontend/src/pages/Member.js
@@ -10,6 +10,8 @@ import RadioGroup from '@mui/material/RadioGroup';
 import MemberTable from '../components/MemberTable';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import MemberCard from '../components/MemberCard';
+import ProjectCard from '../components/ProjectCard';
+
 
 const Member = ({ projectName }) => {
   // TODO: Replace with some default data
@@ -30,6 +32,23 @@ const Member = ({ projectName }) => {
       name: '李四',
       avatar: 'https://avatars0.githubusercontent.com/u/8186664?s=460&v=4',
       description: '这个人很懒，什么都没有留下',
+    },
+  ]);
+
+  const [projects, setProjects] = useState([
+    {
+      name: 'blockchainCourse',
+      description: '浙江大学 区块链与数字货币课程',
+      major: 'javascript',
+      star: '20',
+      lastupdate: 'Updated on 29 Jun 2020'
+    },
+    {
+      name: 'cCompiler',
+      description: '这个人很懒，什么都没有留下',
+      major: 'javascript',
+      star: '20',
+      lastupdate: 'Updated on 29 Jun 2020'
     },
   ]);
 
@@ -60,6 +79,10 @@ const Member = ({ projectName }) => {
     <MemberCard key={member.name} {...member} />
   ));
 
+  const projectCards = projects.map((project) => (
+    <ProjectCard key={project.name} {...project} />
+  ));
+
   return (
     <Layout>
       <Grid item container direction='column' xs={12} sm={4}>
@@ -73,7 +96,10 @@ const Member = ({ projectName }) => {
       <Grid item container direction='column' xs={12} sm={4}>
         {memberCards}
       </Grid>
-      <Grid item container direction='column' xs={12} sm={4}></Grid>
+      <Grid item container direction='column' xs={12} sm={4}>
+        {projectCards}
+
+      </Grid>
     </Layout>
   );
 };
