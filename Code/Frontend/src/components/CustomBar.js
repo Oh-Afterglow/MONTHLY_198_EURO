a91340c7407ed67d6a41c1fc5b206c442ce7b6d3
enzymii <enzymiix@gmail.com>
2021-12-26 18:13:08 +0800

    🔨  Fix custom chart styles and routing for navbar and app bar and main page

--- Code/Frontend/src/components/CustomBar.js
+++ Code/Frontend/src/components/CustomBar.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
-
-const CustomBar = ({ data}) => {
-
-    return (
-        
-        <BarChart width={730} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
-    );
-}
+const CustomBar = ({ data, width, height }) => {
+  return (
+    <BarChart
+      {...{ width, height: Math.trunc(height * 0.5) }}
+      style={{ marginTop: '25%' }}
+      data={data}
+    >
+      <CartesianGrid strokeDasharray='3 3' />
+      <XAxis dataKey='name' />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey='value' fill='#8884d8' />
+    </BarChart>
+  );
+};
 
 export default CustomBar;
